b9d022be72be85985141a767ef3cc55ec6990350
Sarah <sarrah9573@gmail.com>
2024-08-25 17:01:56 +0300

    пытки

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import "./media.css";
 import img from "./img/sadik.jpg";
 import { getAbout } from "./about";
 import { MenuItem } from "./menu";
@@ -8,22 +9,28 @@ import { getContact } from "./contact";
 let body = document.querySelector("body");
 body.classList.add("container");
 body.className = "container";
+
 const header = document.createElement("header");
 header.classList.add("header");
 body.append(header);
+
+const leftDiv = document.createElement('div');
+leftDiv.classList.add('leftDiv');
 const h1 = document.createElement("h1");
 h1.textContent = "Добро пожаловать в наш ресторан!";
 h1.classList.add("h1");
-header.append(h1);
+leftDiv.append(h1)
+
 
 const firstDiv = document.createElement("div");
+firstDiv.classList.add('firstDiv')
 firstDiv.className = "firstDiv";
-header.append(firstDiv);
+
 const aboutBtn = document.createElement("button");
 aboutBtn.className = "aboutBtn";
-
 aboutBtn.textContent = "О нас";
 aboutBtn.classList.add("about");
+
 const menuBtn = document.createElement("button");
 menuBtn.className = "menuBtn";
 menuBtn.textContent = "Меню";
@@ -31,10 +38,13 @@ menuBtn.textContent = "Меню";
 const contactBtn = document.createElement("button");
 contactBtn.className = "contactBtn";
 contactBtn.textContent = "Контакты";
+
 const resetBtn = document.createElement("button");
 resetBtn.classList.add("close-button");
 resetBtn.textContent = "Закрыть";
 firstDiv.append(aboutBtn, menuBtn, contactBtn, resetBtn);
+header.append(leftDiv);
+header.append(firstDiv)
 
 resetBtn.addEventListener("click", () => {
   location.reload();
